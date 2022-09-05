68352ae4c787108c08a83d207ebe3c8bf9e4486a
Isidro Salcedo <salcedo.isidro@gmail.com>
2022-09-05 14:58:11 -0700

    wip

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,11 +5,20 @@ export const About = (): JSX.Element => {
   return (
     <Layout
       customMeta={{
-        title: 'About - Isidro Salcedo',
+        title: 'About - Isidro Salcedo'
       }}
     >
-      <h1>About Page</h1>
-      <p>Welcome to the about page</p>
+      <h1>About Me</h1>
+      <p>My name is Isidro Salcedo. I have been a software engineer for over 10 years, building tools for streaming platforms.</p>
+      <p>I recently made the transition to the fintech industry where I am working on blockchain applications.</p>
+
+      <div style={{ marginTop: '46px' }}>
+        <a
+          href="https://twitter.com/issalcedo"
+        >
+          Twitter
+        </a>
+      </div>
     </Layout>
   );
 };
